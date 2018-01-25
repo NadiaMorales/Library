5d81878fb87d58dd22c712cc0c4bd0fa646d0228
keslymartinez <keslymartinezvaldes@gmail.com>
2018-01-25 16:34:37 -0300

    agregando tamaños con js

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,25 @@
-'use strict'
+'use string';
+
+const img = document.getElementsByClassName('img-s');
+for (i = 0; i < img.length; i++) {
+  img[i].style.width = '10em';
+  img[i].style.margin = '2em';
+
+}
 
 
-const img = document.querySelectorAll('img')
-console.log(img);
+const img2 = document.getElementsByClassName('img-m');
+for (i = 0; i < img2.length; i++) {
+	img2[i].style.width = '15em';
+	img2[i].style.margin = '2em';
 
-img.style.width = '10em';
+}
 
 
+const img3 = document.getElementsByClassName('img-m');
+for (i = 0; i < img3.length; i++) {
+	img3[i].style.width = '15em';
+	img3[i].style.margin = '2em';
+
+}
 
