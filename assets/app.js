2c215652dec6c661c68f82700f6af82c4517a2e7
AyitaXD <nadia.morales.leiva@gmail.com>
2018-01-26 16:59:17 -0300

    actualizando index

--- assets/app.js
+++ assets/app.js
@@ -1,25 +1,36 @@
 'use string';
 
-const img = document.getElementsByClassName('img-s');
+const img = document.getElementsByClassName('img-xs');
 for (i = 0; i < img.length; i++) {
-  img[i].style.width = '10em';
-  img[i].style.margin = '2em';
+  img[i].style.width = '30em';
+  img[i].style.margin = '5px';
 
 }
-
-
-const img2 = document.getElementsByClassName('img-m');
+const img2 = document.getElementsByClassName('img-s');
 for (i = 0; i < img2.length; i++) {
-	img2[i].style.width = '15em';
-	img2[i].style.margin = '2em';
+	img2[i].style.width = '30em';
+	img2[i].style.margin = '5px';
 
 }
-
 
 const img3 = document.getElementsByClassName('img-m');
 for (i = 0; i < img3.length; i++) {
-	img3[i].style.width = '15em';
-	img3[i].style.margin = '2em';
+	img3[i].style.width = '30em';
+	img3[i].style.margin = '5px';
+	img3[i].style.height = '30em';
+
 
 }
+const img4 = document.getElementsByClassName('img-l');
+for (i = 0; i < img4.length; i++) {
+	img4[i].style.width = '24em';
+	img4[i].style.margin = '5px';
+	img4[i].style.height = '22em';
 
+}
+const img5 = document.getElementsByClassName('img-xl');
+for (i = 0; i < img5.length; i++) {
+	img5[i].style.width = '24em';
+	img5[i].style.margin = '2em';
+
+}
