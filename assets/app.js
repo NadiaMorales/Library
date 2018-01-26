65077c9f81d1f5c00942c350a3db4fa7a81b4ca1
keslymartinez <keslymartinezvaldes@gmail.com>
2018-01-26 17:41:49 -0300

    Generando Columas con js

--- assets/app.js
+++ assets/app.js
@@ -1,37 +1,33 @@
 'use string';
+ const  cont = document.getElementsByClassName('gallery');
+for (i = 0; i < cont.length; i++){
+cont[i].style.columnCount = 3; // Generador de Columnas
+}
 
 const img = document.getElementsByClassName('img-xs');
 for (i = 0; i < img.length; i++) {
-  img[i].style.width = '30em';
-  img[i].style.margin = '5px';
-
+  img[i].style.width = '100%';
+  img[i].style.padding = '7px';
 }
 const img2 = document.getElementsByClassName('img-s');
 for (i = 0; i < img2.length; i++) {
-	img2[i].style.width = '30em';
-	img2[i].style.margin = '5px';
-
+	img2[i].style.width = '100%';
+	img2[i].style.padding = '7px';
 }
-
 const img3 = document.getElementsByClassName('img-m');
 for (i = 0; i < img3.length; i++) {
-	img3[i].style.width = '30em';
-	img3[i].style.margin = '5px';
+	img3[i].style.width = '100%';
+	img3[i].style.padding = '7px';
 	img3[i].style.height = '30em';
-
-
 }
 const img4 = document.getElementsByClassName('img-l');
 for (i = 0; i < img4.length; i++) {
-	img4[i].style.width = '24em';
-	img4[i].style.margin = '5px';
+	img4[i].style.width = '100%';
+	img4[i].style.padding = '7px';
 	img4[i].style.height = '22em';
-
 }
 const img5 = document.getElementsByClassName('img-xl');
 for (i = 0; i < img5.length; i++) {
-	img5[i].style.width = '24em';
-	img5[i].style.margin = '2em';
-
+	img5[i].style.width = '100%';
+	img5[i].style.padding = '7px';
 }
- 
