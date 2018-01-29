b396f70645d21605f50f86791226cb7b3011bc85
keslymartinez <keslymartinezvaldes@gmail.com>
2018-01-29 15:17:44 -0300

    modificando tamaños

--- assets/app.js
+++ assets/app.js
@@ -2,30 +2,30 @@
 // tamaños para imagenes
 const img = document.getElementsByClassName('img-xs');
 for (i = 0; i < img.length; i++) {
-  img[i].style.width = '100%';
-  img[i].style.padding = '7px';
+  img[i].style.width = '20em';
+  // img[i].style.padding = '7px';
 }
 const img2 = document.getElementsByClassName('img-s');
 for (i = 0; i < img2.length; i++) {
-  img2[i].style.width = '100%';
-  img2[i].style.padding = '7px';
+  img2[i].style.width = '25em';
+  //img2[i].style.padding = '7px';
 }
 const img3 = document.getElementsByClassName('img-m');
 for (i = 0; i < img3.length; i++) {
-  img3[i].style.width = '100%';
-  img3[i].style.padding = '7px';
-  img3[i].style.height = '30em';
+  img3[i].style.width = '30em';
+ // img3[i].style.padding = '7px';
+  img3[i].style.height = 'auto';
 }
 const img4 = document.getElementsByClassName('img-l');
 for (i = 0; i < img4.length; i++) {
-  img4[i].style.width = '100%';
-  img4[i].style.padding = '7px';
-  img4[i].style.height = '22em';
+  img4[i].style.width = '40em';
+// img4[i].style.padding = '7px';
+  img4[i].style.height = 'auto';
 }
 const img5 = document.getElementsByClassName('img-xl');
 for (i = 0; i < img5.length; i++) {
-  img5[i].style.width = '100%';
-  img5[i].style.padding = '7px';
+  img5[i].style.width = '45em';
+//  img5[i].style.padding = '7px';
 }
 
 // funcion que agranda una imagen
@@ -39,5 +39,5 @@ for (var i = 0; i < imgHover.length; i++) {
 // galeria de imagenes
 const cont = document.getElementsByClassName('gallery');
 for (i = 0; i < cont.length; i++) {
-  cont[i].style.columnCount = 3; // Generador de Columnas
+  cont[i].style.columnCount = 5; // Generador de Columnas
 }
